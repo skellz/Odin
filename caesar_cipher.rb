21b71eaf9d544a17a37fbbf3cf3d23a53393485b
skellz <sherwin.kelly@gmail.com>
2014-03-03 17:10:29 -0500

    added regex for special characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -7,7 +7,7 @@ class CaesarCipher
 
   def create_cipher
     @input.each_with_index do |x,index|
-      unless x =~ /[a-zA-Z]/
+      if x =~ /[a-zA-Z]/
         a = (@alphabet.index(x.downcase) + @val) % 26
         a -= 1 if a == 0 || a == 1
         x =~ /[A_Z]/ ? @input[index] = @alphabet[a].upcase : @input[index] = @alphabet[a]
