caff795b84bc1f86d273f7d8ae0e45d0aa83f4fd
skellz <sherwin.kelly@gmail.com>
2014-03-03 17:04:45 -0500

    first caesar_cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+class CaesarCipher
+  def initialize(input, val)
+    @input = input.split('')
+    @val = val.to_i
+    @alphabet = ('a'..'z').to_a
+  end
+
+  def create_cipher
+    @input.each_with_index do |x,index|
+      unless x == ' '
+        a = (@alphabet.index(x.downcase) + @val) % 26
+        a -= 1 if a == 0 || a == 1
+        x =~ /[A_Z]/ ? @input[index] = @alphabet[a].upcase : @input[index] = @alphabet[a]
+      end
+    end
+    @input.join
+  end
+end
